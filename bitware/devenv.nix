390716a4f49c977b6fd4b62f80e881e6115faaa2
hf-ikea <bluehairguyiscool@gmail.com>
2025-02-10 22:44:35 -0800

    setup devenv proper for ecp5

---
+++ bitware/devenv.nix
@@ -0,0 +1,15 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  packages = with pkgs; [
+    git
+    iverilog
+    yosys
+    trellis
+    nextpnr
+  ];
+}
